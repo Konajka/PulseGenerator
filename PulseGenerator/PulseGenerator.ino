0cd373789286b8583913daff17a59c741c81fa4f
Tomáš Borek <tomas.borek@artiisgroup.cz>
2019-05-02 12:18:04 +0200

    Created project for Arduino IDE.

---
+++ PulseGenerator/PulseGenerator.ino
@@ -0,0 +1,29 @@
+/*
+    Pulse Generator
+
+    Simple flip-flop pulse generator with adjustable minimal, maximal and
+    current working frequency. Adjusting is perfomed by rotary encoder,
+    shown on display.
+
+    The circuit:
+    * Arduino Nano
+    * Rotary encoder KY-040
+    * I2C OLED display 0,96" 128x64 (SSD-1306)
+
+    Created 2019-05-02 By Yoda-81
+    Modified 2019-05-03 By Yoda-81
+
+    https://github.com/Yoda-81
+
+*/
+
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
