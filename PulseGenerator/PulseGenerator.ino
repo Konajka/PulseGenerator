5367a0b31f1186f3267c51280353fb7c6f04d5fe
Tomáš Borek <tomas.borek@post.cz>
2019-05-02 22:45:06 +0200

    added rotary encoder test code

--- PulseGenerator/PulseGenerator.ino
+++ PulseGenerator/PulseGenerator.ino
@@ -19,11 +19,61 @@
 
 #include <Arduino.h>
 
+#define ENCODER_CLK 5
+#define ENCODER_DT 4
+#define ENCODER_SW 3
+
+int pinClkRetain;
+int pinDtRetain;
+int pinSwRetain;
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(9600);
+    Serial.println("Rotary encoder");
+    
+    pinMode(ENCODER_CLK, INPUT);
+    pinMode(ENCODER_DT, INPUT);
+    pinMode(ENCODER_SW, INPUT_PULLUP);
+    
+    pinClkRetain = digitalRead(ENCODER_CLK);
+    pinDtRetain = digitalRead(ENCODER_DT);
+    pinSwRetain = digitalRead(ENCODER_SW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    int pinClk = digitalRead(ENCODER_CLK);
+    int pinDt = digitalRead(ENCODER_DT);
+
+    if (pinClk != pinClkRetain || pinDt != pinDtRetain) {
+        if (pinDt == pinClkRetain && pinClk != pinDtRetain) {
+            Serial.println("<-");
+        } else if (pinClk == pinDtRetain && pinDt != pinClkRetain) {
+            Serial.println("->");
+        }
+        pinClkRetain = pinClk;
+        pinDtRetain = pinDt;
+    }
+
+    /*
+    if (pinClkRetain != pinClk) {
+        pinClkRetain = pinClk;
+        if (pinClk == HIGH) {
+            int pinDt = digitalRead(ENCODER_DT);
+            if (pinClk == pinDt) {
+                Serial.println("->");
+            } else {
+                Serial.println("<-");
+            }
+        }
+    }*/
+
+    int pinSw = digitalRead(ENCODER_SW);
+    if (pinSwRetain != pinSw) {
+        pinSwRetain = pinSw;
+        if (pinSw == HIGH) {
+            Serial.println("[ ]");
+        } else {
+            Serial.println("[X]");
+        }
+    }
 }
